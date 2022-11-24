7f66e1bf53b04fa5b0261b09a69184bb257132c8
OmurRifat <jabedomur.ece.ku@gmail.com>
2022-11-24 19:03:40 +0600

    some changes added

--- src/Pages/Home/Feature/Feature.js
+++ src/Pages/Home/Feature/Feature.js
@@ -4,8 +4,8 @@ import Button from '../../../Components/Button/Button';
 
 const Feature = () => {
     return (
-        <div className='flex items-center justify-around'>
-            <div className=" flex-1">
+        <div className='flex items-center justify-around my-32'>
+            <div className=" flex-1 mb">
                 <h4 className=' text-center text-lg md:text-2xl font-semibold'>Here You get all you need<br /> for your <span className='text-primary'>Vision.</span></h4>
                 <p>
                     This is platform for the camera lovers. Here you will get types of second hand camera. This site can be called as a vision fulfillment site.This is platform for the camera lovers. Here you will get types of second hand camera. This site can be called as a vision fulfillment site.
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+import { format } from 'date-fns/esm';
 import React from 'react';
 import Advertisements from '../Advertisements/Advertisements';
 import Banner from '../Banner/Banner';
@@ -5,6 +7,12 @@ import Feature from '../Feature/Feature';
 import Products from '../Products/Products';
 
 const Home = () => {
+    // const date = new Date();
+    // const newDate = format(date, ("PP"));
+    // console.log(JSON.stringify(newDate))
+    // axios.get('products.json')
+    //     .then(res => console.log(res.data))
+    //     .catch(err => console.log(err))
     return (
         <div className=' min-h-screen w-4/5 mx-auto'>
             <Banner></Banner>
