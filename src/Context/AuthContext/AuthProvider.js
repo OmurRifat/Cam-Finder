e9e01fa0ccda95c3a5dba7f49492763e3f1ef08c
OmurRifat <jabedomur.ece.ku@gmail.com>
2022-11-24 09:03:14 +0600

    some configuration added

--- src/Context/AuthContext/AuthProvider.js
+++ src/Context/AuthContext/AuthProvider.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { createContext } from 'react';
+import { getAuth } from 'firebase/auth'
+import { app } from '../../firebase/firebase.config';
 
+export const AuthContext = createContext();
+const auth = getAuth(app)
 const AuthProvider = ({ children }) => {
+    const authInfo = {
+
+    }
     return (
-        <div>
+        <AuthContext.Provider value={ authInfo }>
             {
                 children
             }
-        </div>
+        </AuthContext.Provider >
     );
 };
 
--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBX1SrwFpOA75JD8ti_jkvLGsIUXKfYwww",
-    authDomain: "cam-finder.firebaseapp.com",
-    projectId: "cam-finder",
-    storageBucket: "cam-finder.appspot.com",
-    messagingSenderId: "66797003630",
-    appId: "1:66797003630:web:a0eeaa9a9b0484ae25977f"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
