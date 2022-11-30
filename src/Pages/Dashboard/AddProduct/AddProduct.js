6b09c1c430116fa4fef9bee804590ab292108439
OmurRifat <jabedomur.ece.ku@gmail.com>
2022-11-30 15:27:15 +0600

    Add a Product section updated

--- src/Pages/Dashboard/AddProduct/AddProduct.js
+++ src/Pages/Dashboard/AddProduct/AddProduct.js
@@ -1,84 +1,99 @@
+import axios from 'axios';
+import { format } from 'date-fns';
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
 
 const AddProduct = () => {
     const { register, handleSubmit } = useForm();
 
-    const onSubmit = data => {
-        console.log(data);
+
+    const handleAddProduct = (data, e) => {
+        const date = new Date();
+        const postedTime = format(date, "PP");
+        const image = data.imgURL[0];
+        const formData = new FormData();
+        formData.append('image', image);
+        const url = `https://api.imgbb.com/1/upload?key=2d04212c49adf357600821c10c5e3dd0`;
+        fetch(url, {
+            method: 'POST',
+            body: formData
+        })
+            .then(res => res.json())
+            .then(imgData => {
+                if (imgData.success) {
+                    data.imgURL = `${imgData.data.url}`;
+                    data.postedTime = postedTime;
+
+
+                    axios.post('http://localhost:5000/products', data)
+                        .then(res => {
+                            if (res.status === 200)
+                                toast.success("Product Added Sucessfully")
+                        })
+                        .catch(err => toast.error(err.message))
+
+                }
+            })
+        e.target.reset();
     }
     return (
         <div className=' bg-white m-6 rounded-lg px-6 py-10'>
             <h4 className='text-lg text-primary font-bold text-center mb-4'>Add a Product</h4>
-            <form >
+            <form onSubmit={ handleSubmit(handleAddProduct) }>
                 <div className="grid md:grid-cols-2 md:gap-6">
                     <div className="relative z-0 mb-6 w-full group">
-                        <input type="text" { ...register("name") } name="book_name" id="book_name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-primary focus:outline-none focus:ring-0 focus:border-primary peer" placeholder=" " required="" />
-                        <label htmlFor="book_name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-primary peer-focus:dark:text-primary peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Book Name</label>
+                        <input type="text" { ...register('name') } placeholder="Product Name" className="input input-bordered input-primary w-full" />
                     </div>
                     <div className="relative z-0 mb-6 w-full group">
-                        <select className=" w-full select">
-                            <option disabled defaultValue={ "Category" } className=' text-gray-900'>Category</option>
+                        <input type="file" { ...register('imgURL') } className="file-input file-input-bordered file-input-primary w-full" />
+                    </div>
+                </div>
+                <div className="grid md:grid-cols-1 md:gap-6">
+                    <div className="relative z-0 mb-6 w-full group">
+                        <select { ...register('categoryId') } className="select select-primary w-full ">
+                            <option disabled selected>Category</option>
                             <option>1</option>
                             <option>2</option>
                             <option>3</option>
                         </select>
-                        <hr className=' bg-black' />
                     </div>
                 </div>
                 <div className="grid md:grid-cols-2 md:gap-6">
                     <div className="relative z-0 mb-6 w-full group">
-                        <input type="text" name="publisher" id="publisher" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-primary focus:outline-none focus:ring-0 focus:border-primary peer" placeholder=" " required="" />
-                        <label htmlFor="publisher" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-primary peer-focus:dark:text-primary peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Publisher</label>
+                        <input type="text" { ...register('location') } placeholder="Location" className="input input-bordered input-primary w-full" />
                     </div>
                     <div className="relative z-0 mb-6 w-full group">
-                        <input type="boolean" name="stock" id="stock" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-primary focus:outline-none focus:ring-0 focus:border-primary peer" placeholder=" " required="" />
-                        <label htmlFor="stock" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-primary peer-focus:dark:text-primary peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">In Stock?</label>
+                        <input type="number" { ...register('resalePrice') } placeholder="Resale Price" className="input input-bordered input-primary w-full" />
                     </div>
                 </div>
                 <div className="grid md:grid-cols-2 md:gap-6">
                     <div className="relative z-0 mb-6 w-full group">
-                        <input type="text" name="price" id="price" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-primary focus:outline-none focus:ring-0 focus:border-primary peer" placeholder=" " required="" />
-                        <label htmlFor="price" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-primary peer-focus:dark:text-primary peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Price</label>
+                        <input type="number" { ...register('originalPrice') } placeholder="Original Price" className="input input-bordered input-primary w-full" />
                     </div>
                     <div className="relative z-0 mb-6 w-full group">
-                        <input type="text" name="photoURL" id="photoURL" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-primary focus:outline-none focus:ring-0 focus:border-primary peer" placeholder=" " required="" />
-                        <label htmlFor="photoURL" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-primary peer-focus:dark:text-primary peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Photo URL</label>
+                        <input type="number" { ...register('yearOfUse') } placeholder="Year of Use" className="input input-bordered input-primary w-full" />
                     </div>
                 </div>
                 <div className="grid md:grid-cols-2 md:gap-6">
                     <div className="relative z-0 mb-6 w-full group">
-                        <input type="text" name="price" id="price" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-primary focus:outline-none focus:ring-0 focus:border-primary peer" placeholder=" " required="" />
-                        <label htmlFor="price" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-primary peer-focus:dark:text-primary peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Price</label>
+                        <input type="text" { ...register('sellerName') } placeholder="Seller Name" className="input input-bordered input-primary w-full" />
                     </div>
                     <div className="relative z-0 mb-6 w-full group">
-                        <input type="text" name="photoURL" id="photoURL" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-primary focus:outline-none focus:ring-0 focus:border-primary peer" placeholder=" " required="" />
-                        <label htmlFor="photoURL" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-primary peer-focus:dark:text-primary peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Photo URL</label>
+                        <input type="text" { ...register('condition') } placeholder="Condition" className="input input-bordered input-primary w-full" />
                     </div>
                 </div>
+
                 <div className="grid md:grid-cols-2 md:gap-6">
                     <div className="relative z-0 mb-6 w-full group">
-                        <input type="text" name="price" id="price" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-primary focus:outline-none focus:ring-0 focus:border-primary peer" placeholder=" " required="" />
-                        <label htmlFor="price" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-primary peer-focus:dark:text-primary peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Price</label>
+                        <input type="text" { ...register('sellerNumber') } placeholder="Seller Number" className="input input-bordered input-primary w-full" />
                     </div>
                     <div className="relative z-0 mb-6 w-full group">
-                        <input type="text" name="photoURL" id="photoURL" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-primary focus:outline-none focus:ring-0 focus:border-primary peer" placeholder=" " required="" />
-                        <label htmlFor="photoURL" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-primary peer-focus:dark:text-primary peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Photo URL</label>
-                    </div>
-                </div>
-                <div className="grid md:grid-cols-2 md:gap-6">
-                    <div className="relative z-0 mb-6 w-full group">
-                        <input type="text" name="price" id="price" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-primary focus:outline-none focus:ring-0 focus:border-primary peer" placeholder=" " required="" />
-                        <label htmlFor="price" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-primary peer-focus:dark:text-primary peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Price</label>
-                    </div>
-                    <div className="relative z-0 mb-6 w-full group">
-                        <input type="text" name="photoURL" id="photoURL" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-primary focus:outline-none focus:ring-0 focus:border-primary peer" placeholder=" " required="" />
-                        <label htmlFor="photoURL" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-primary peer-focus:dark:text-primary peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Photo URL</label>
+                        <input type="text" { ...register('yearOfPurchase') } placeholder="Year of Purchase" className="input input-bordered input-primary w-full" />
                     </div>
                 </div>
                 <div className="relative z-0 mb-6 w-full group">
-                    <input type="text" name="repeat_password" id="details" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-primary focus:outline-none focus:ring-0 focus:border-primary peer" placeholder=" " required="" />
-                    <label htmlFor="details" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-primary peer-focus:dark:text-primary peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Book Details</label>
+                    <textarea className="textarea textarea-primary w-full" { ...register('description') } placeholder="Write down the description of the Product."></textarea>
                 </div>
                 <input type="submit" value="Add" className='btn btn-primary' />
             </form>
