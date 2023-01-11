3bb36b939fd998fae05f0e64a1f86c7b36add5b8
OmurRifat <jabedomur.ece.ku@gmail.com>
2023-01-11 18:50:15 +0600

    some changes added

--- src/Pages/Shared/PageNotFound/PageNotFound.js
+++ src/Pages/Shared/PageNotFound/PageNotFound.js
@@ -3,6 +3,7 @@ import './PageNotFound.css'
 import { Link } from 'react-router-dom';
 
 const PageNotFound = () => {
+
     return (
         <div className='not-found min-h-screen relative'>
             <Link to='/'><button className='btn btn-primary absolute bottom-1/2 right-1/4'>Back to Home Screen</button></Link>
